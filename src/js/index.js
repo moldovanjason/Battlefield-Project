d704871b1af1a96f9aa2f8eb1145c8ff6eee3751
moldovanjason <moldovanjason@gmail.com>
2020-07-23 19:50:11 +0000

    first upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,28 @@
-/* eslint-disable */
+var battlefield = document.querySelector(".container");
+const fire = () => {
+  battlefield.childNodes[Math.floor(Math.random() * 10)].childNodes[
+    Math.floor(Math.random() * 10)
+  ].style.background = "red";
+};
 
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
-import "../style/index.scss";
-
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  for (var i = 0; i < 10; i++) {
+    var row = document.createElement("div");
+    battlefield.appendChild(row);
+    row.style.display = "flex";
+    for (var k = 0; k < 10; k++) {
+      var cell = document.createElement("div");
+      row.appendChild(cell);
+      cell.innerHTML = Math.floor(Math.random() * 2);
+      cell.style.border = "2px solid gray";
+      cell.style.padding = "15px";
+      if (cell.innerHTML == 1) cell.style.background = "gray";
+    }
+  }
+  var button = document.createElement("button");
+  button.innerHTML = "Fire";
+  button.style.padding = "10px 30px";
+  button.style.background = "red";
+  battlefield.appendChild(button);
+  button.addEventListener("click", fire);
 };
